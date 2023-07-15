946db6f61e1403fb9ab3d700c95ad6de4762d2c5
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-07-16 01:23:17 +0400

    create my_portfolios table

---
+++ database/migrations/2023_07_15_210124_create_my_portfolios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('my_portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->string('view_title')->nullable();
+            $table->string('inside_title')->nullable();
+            $table->text('short_description')->nullable();
+            $table->text('long_description')->nullable();
+            $table->string('view_image')->nullable();
+            $table->string('view_alt_image')->nullable();
+            $table->string('inside_image')->nullable();
+            $table->string('inside_alt_image')->nullable();
+            $table->string('link')->nullable();
+            $table->string('visibility')->nullable();
+            $table->string('smaill_inside_title')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('my_portfolios');
+    }
+};
